ded453b94d07831b79213d3b36f2839cd1ef0620
kai <b10223052@yuntech.org.tw>
2019-07-10 22:58:59 +0800

    add router bar style

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,51 +1,57 @@
 import React,{Component} from 'react'
-import { Link, Route,Switch } from 'react-router-dom'
+import { Link, Route,Switch, NavLink, isActive } from 'react-router-dom'
 import loadComponent from './loadComponent.js'
-import closeButton from '../icon/close.png'
-import ListIcon from '../icon/todo-list.png'
-import AnalyticsIcon from '../icon/analytics.png'
-import RingtonesIcon from '../icon/ringtones.png'
-import test from '../icon/baseline-close-24px.svg'
+import TodoIcon from '../components/Icon/TodoIcon.js'
+import AnalyticsIcon from '../components/Icon/AnalyticsIcon.js'
+import RingtonesIcon from '../components/Icon/RingtonesIcon.js'
+import CloseIcon from '../components/Icon/CloseIcon.js'
+
 
 const Main = loadComponent({loader:()=>import('../pages/Main')})
 const TodoList = loadComponent({loader:()=>import('../pages/TodoList')})
 const Analytics = loadComponent({loader:()=>import('../pages/Analytics')})
 const Ringtones = loadComponent({loader:()=>import('../pages/Ringtones')})
 
-
+console.log(isActive)
 class Layout extends Component{
-  render(){
-    return(
-      <div className='content'>
-        <div className='menu-bar'>
-          <div className='link'>
-            <img src={ListIcon}></img>
-            <Link to='/todo-list'>to-do list</Link>
-          </div>
-          <div className='link'>
-            <img src={AnalyticsIcon}></img>
-            <Link to='/analytics'>analytics</Link>
-          </div>
-          <div className='link'>
-            <img src={RingtonesIcon}></img>
-            <Link to='/ringtones'>ringtones</Link>
-          </div>
-        </div>
-        <div className='container'>
-          <Route exact path='/' component={Main}/>
-          <Route path='/todo-list' component={TodoList}/>
-          <Route path='/analytics' component={Analytics}/>
-          <Route path='/ringtones' component={Ringtones}/>
-        </div>
-        <div className='logo-bar'>
-          <Link to='/'>
-            <img src={closeButton}/>
-          </Link>
-          <p>pomodoro</p>
-        </div>
-      </div>
-    )
-  }
+	render(){
+		return(
+			<div className="content">
+				<ul className="menu-bar">
+					<li className="link">
+						<NavLink activeClassName="active" to="/todo-list">
+							<TodoIcon />
+							to-do list
+						</NavLink>
+					</li>
+					<li className="link">
+						<NavLink activeClassName="active" to="/analytics">
+							<AnalyticsIcon />
+							analytics
+						</NavLink>
+					</li>
+					<li className="link">
+						<NavLink activeClassName="active" to="/ringtones">
+							<RingtonesIcon/>
+							ringtones
+						</NavLink>
+					</li>
+				</ul>
+				<div className="container">
+					<Route exact path="/" component={Main}/>
+					<Route path="/todo-list" component={TodoList}/>
+					<Route path="/analytics" component={Analytics}/>
+					<Route path="/ringtones" component={Ringtones}/>
+				</div>
+				<div className="logo-bar">
+					<Link to="/">
+						<CloseIcon/>
+					</Link>
+					<p>pomodoro</p>
+				</div>
+			</div>
+		)
+	}
 }
 
 
