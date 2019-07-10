58c61c89d167b46f6c432ede8a690521fed00be0
kai <b10223052@yuntech.org.tw>
2019-07-10 22:59:35 +0800

    add image-loader url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,16 @@ module.exports = {
         'css-loader', // translates CSS into CommonJS
         'sass-loader' // compiles Sass to CSS, using Node Sass by default
       ]
-    }, ]
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: { limit: 40000 }
+        },
+        'image-webpack-loader'
+      ]
+    }]
   },
   resolve: {
     modules: [path.resolve(__dirname, 'src'), 'node_modules']
