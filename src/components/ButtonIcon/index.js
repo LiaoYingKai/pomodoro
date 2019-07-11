81176ef7503569218cd8042ab4a8f1417d6694f2
kai <b10223052@yuntech.org.tw>
2019-07-11 10:14:40 +0800

    add button-icon enums

--- src/components/ButtonIcon/index.js
+++ src/components/ButtonIcon/index.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { Component, } from 'react'
+import PropTypes from 'prop-types'
 import './style.scss'
-const PlayIcon = ({color,type})=>(
-  <div className={`play-icon ${color}`}>
-    <div className={type}></div>
-  </div>
-)
-export default PlayIcon
+
+const ButtonIconEnums={
+  DEFAULT: 'default',
+  START: 'start',
+  CHOOSED: 'choosed',
+  CHECKED: 'checked'
+}
+
+const propTypes = {
+  type: PropTypes.oneOf(Object.values(ButtonIconEnums).concat(''))
+}
+const defaultProps = {
+  type: ButtonIconEnums.DEFAULT,
+}
+
+class ButtonIcon extends Component{
+
+  _renderButton(){
+    const { type } = this.props
+
+    switch(type){
+    case ButtonIconEnums.DEFAULT:
+      return(
+        <div className="button__icon">
+          <div></div>
+        </div>
+      )
+    case ButtonIconEnums.START:
+      return(
+        <div className="button__icon">
+          <div className="play"></div>
+        </div>
+      )
+    case ButtonIconEnums.CHOOSED:
+      return(
+        <div className="button__icon pink">
+          <div className="choosed"></div>
+        </div>
+      )
+    case ButtonIconEnums.CHECKED:
+      return(
+        <div className="button__icon">
+          <div className="checked"></div>
+        </div>
+      )
+    default:
+    }
+  }
+  render(){
+    return (
+      <React.Fragment>
+        {
+          this._renderButton()
+        }
+      </React.Fragment>
+    )
+  }
+}
+ButtonIcon.ButtonIconEnums = ButtonIconEnums
+ButtonIcon.propTypes = propTypes
+ButtonIcon.defaultProps = defaultProps
+
+export default ButtonIcon
