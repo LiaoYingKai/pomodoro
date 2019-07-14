f7ecc0e4cf72f74fddc3d5465f57589ef6cd7b20
kai <b10223052@yuntech.org.tw>
2019-07-14 23:43:59 +0800

    add button and modify style

--- src/components/InputTodo/InputTodo.jsx
+++ src/components/InputTodo/InputTodo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const InputTodo = ({className}) =>(
-  <div className='input-todo'>
+const InputTodo = ({className, onClick}) =>(
+  <div className='input__todo'>
     <input type='text' placeholder='ADD A NEW MISSINO...' className={className}/>
+    <button onClick={onClick}> + </button>
   </div>
 )
 
