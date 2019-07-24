950eb105353ea3f5aeaa524b5220e1b6a6940121
kai <b10223052@yuntech.org.tw>
2019-07-24 23:14:04 +0800

    add Main page stateEnum

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import AnalyticsIcon from '../../components/Icon/AnalyticsIcon.js'
@@ -7,9 +8,50 @@ import TimePicker from '../../components/TimePicker'
 import ButtonIcon from '../../components/ButtonIcon'
 import InputTodo from '../../components/InputTodo'
 import Todo from '../../components/Todo'
+import { connect } from 'react-redux'
+import { changeTodoState, deleteTodo,  } from '../../actions/todoActions'
+import { setDoing } from '../../actions/doingActions'
+
+const StateEnums = {
+  WORK_START: 'work_start',
+  WORK_RUNNING: 'work_running',
+  BREAK_START: 'break_start',
+  BREAK_RUNNING: 'break_running',
+}
+const propTypes={
+  state: PropTypes.oneOf(Object.values(StateEnums).concat('')),
+}
+const defaultProps= {
+  state: StateEnums.WORK_START
+}
+
 class Main extends Component {
+  constructor(){
+    super()
+    this._renderTimePicker = this._renderTimePicker.bind(this)
+  }
+  _renderTimePicker(){
+    const { state } = this.props
+    console.log(state)
+    switch(state){
+      case StateEnums.WORK_START: {
+        return <TimePicker type={TimePicker.TypeEnums.START} color={TimePicker.ColorEnums.DEEP_PINK}/>
+      }
+      case StateEnums.WORK_RUNNING: {
+        return <TimePicker type={TimePicker.TypeEnums.RUNNING} color={TimePicker.ColorEnums.DEEP_PINK}/>
+      }
+      case StateEnums.BREAK_START: {
+        return <TimePicker type={TimePicker.TypeEnums.START} color={TimePicker.ColorEnums.DEEP_AQUA_BLUE}/>
+      }
+      case StateEnums.BREAK_RUNNING: {
+        return <TimePicker type={TimePicker.TypeEnums.RUNNING} color={TimePicker.ColorEnums.DEEP_AQUA_BLUE}/>
+      }
+    }
+  }
   render() {
-    const { isOpen } = this.props
+    const { isOpen, nowDoing, deleteTodo, changeTodoState, setDoing } = this.props
+    const { _renderTimePicker } = this
+
     return (
       <React.Fragment>
         <div className={`home__page ${isOpen?'home__page-open': ''}`}>
@@ -20,7 +62,7 @@ class Main extends Component {
                 <div className="home__page__doing__icon"></div>
                 <div>
                   <div className="home__page__doing__title">
-												the First thing to do today
+                    { nowDoing ? nowDoing.text : ''}
                   </div>
                   <div className="home__page__doing__amount__group">
                     <div className="home__page__doing__amount--done"></div>
@@ -41,12 +83,36 @@ class Main extends Component {
             </div>
           </div>
           <div className={`home__page__time__clock ${isOpen?'home__page-open': ''}`}>
-            <TimePicker type={TimePicker.TypeEnums.START} color={TimePicker.ColorEnums.DEEP_PINK}/>
+            {_renderTimePicker()}
           </div>
         </div>
       </React.Fragment>
     )
   }
 }
+Main.propTypes = propTypes
+Main.defaultProps = defaultProps
+Main.StateEnums = StateEnums
 
-export default Main
+function mapStateToProps(state){
+  return {
+    todos: state.todos,
+    nowDoing: state.todos[state.doing]
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    deleteTodo: (id) =>{
+      dispatch(deleteTodo(id))
+    },
+    changeTodoState: (id) => {
+      dispatch(changeTodoState(id))
+    },
+    setDoing: (id) => {
+      dispatch(setDoing(id))
+    }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Main)
